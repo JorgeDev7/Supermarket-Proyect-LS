9ad8971c17b427bd24e1abd0fe818d31747848c0
JorgeDev7 <jorgehumbertocolindres@gmail.com>
2023-06-12 10:45:46 -0600

    Provider Añadido

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ListadoProvider } from './context/listadoProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ListadoProvider>
+      <App />
+    </ListadoProvider>
   </React.StrictMode>
 )
